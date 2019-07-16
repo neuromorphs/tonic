5b3e18ff16906d6a83b04cabe09fddd35163ef6a
Gregor Lenz <mail@lenzgregor.com>
2019-07-15 22:35:01 -0600

    add refractory period method

--- spike_data_augmentation/functional/__init__.py
+++ spike_data_augmentation/functional/__init__.py
@@ -1,5 +1,12 @@
 from .flip_lr import flip_lr_numpy
 from .flip_ud import flip_ud_numpy
+from .refractory_period import refractory_period_numpy
 from .utils import guess_event_ordering_numpy, is_multi_image
 
-__all__ = [flip_lr_numpy, flip_ud_numpy, guess_event_ordering_numpy, is_multi_image]
+__all__ = [
+    flip_lr_numpy,
+    flip_ud_numpy,
+    refractory_period_numpy,
+    guess_event_ordering_numpy,
+    is_multi_image,
+]
---
+++ spike_data_augmentation/functional/refractory_period.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+from .utils import guess_event_ordering_numpy
+
+
+def refractory_period_numpy(
+    events, sensor_size=(346, 260), ordering=None, refractory_period=5000
+):
+    """
+    Sets a refractory period for each pixel, during which events will be
+    ignored/discarded.
+    We keep events if:
+
+        .. math::
+            t_n - t_(n-1) > t_refrac,
+
+    Arguments:
+    - events - ndarray of shape [num_events, num_event_channels]
+    - sensor_size - size of the sensor that was used [W,H]
+    - ordering - ordering of the event tuple inside of events, if None
+                 the system will take a guess through
+                 guess_event_ordering_numpy. This function requires 't', 'x'
+                 and 'y' to be in the ordering
+    - refractory_period - refractory period for each pixel in micro seconds
+
+    Returns:
+    - events - returns reduced set of events
+    """
+
+    if ordering is None:
+        ordering = guess_event_ordering_numpy(events)
+        assert "t" and "x" and "y" in ordering
+
+    t_index = ordering.find("t")
+    x_index = ordering.find("x")
+    y_index = ordering.find("y")
+
+    events_copy = np.zeros(events.shape)
+    copy_index = 0
+    # initialise timestamp map with negative refractory period
+    timestamp_memory = np.zeros((sensor_size[0], sensor_size[1])) - refractory_period
+
+    for event in events:
+        time_since_last_spike = (
+            event[t_index] - timestamp_memory[int(event[x_index]), int(event[y_index])]
+        )
+        if time_since_last_spike > refractory_period:
+            events_copy[copy_index] = event
+            copy_index = copy_index + 1
+            timestamp_memory[int(event[x_index]), int(event[y_index])] = event[t_index]
+
+    events_copy = np.delete(events_copy, np.arange(copy_index, len(events)), axis=0)
+
+    print(
+        "Refractory period of "
+        + str(refractory_period)
+        + " dropped "
+        + str(len(events) - len(events_copy))
+        + " events."
+    )
+
+    del events
+
+    return events_copy
