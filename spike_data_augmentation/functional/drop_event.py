0c2eac65b1231d94e68b23febf69823d6c3c8e8e
bamsumit <bam_sumit@hotmail.com>
2019-07-17 14:04:34 +0800

    dropout module code

--- spike_data_augmentation/functional/drop_event.py
+++ spike_data_augmentation/functional/drop_event.py
@@ -3,19 +3,6 @@ import numpy as np
 from .utils import guess_event_ordering_numpy, is_multi_image
 
 
-def dropEvent(TD, p):
-    length = len(TD.x)
-    nDrop = int(p * length)
-    ind = np.random.randint(0, length, size=nDrop)
-    dropTD = snn.io.event(1, 1, 1, 1)
-    dropTD.x = np.delete(TD.x, ind)
-    dropTD.y = np.delete(TD.y, ind)
-    dropTD.p = np.delete(TD.p, ind)
-    dropTD.t = np.delete(TD.t, ind)
-
-    return dropTD
-
-
 def drop_event_numpy(events, drop_probability=0.5):
     """
     Randomly drops events with drop_probability.
