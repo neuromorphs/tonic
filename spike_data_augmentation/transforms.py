e4c76f74eb0df73aceda675557b94c6e20cb4900
Gregor Lenz <mail@lenzgregor.com>
2019-09-02 18:23:53 +0200

    add images and multi_image to all transform calls

--- spike_data_augmentation/transforms.py
+++ spike_data_augmentation/transforms.py
@@ -35,53 +35,12 @@ class Compose(object):
 
 
 class Crop(object):
-    def __init__(self, images=None, target_size=(256, 256), multi_image=None):
-        self.images = images
+    def __init__(self, target_size=(256, 256)):
         self.target_size = target_size
-        self.multi_image = multi_image
 
-    def __call__(self, events, sensor_size, ordering):
+    def __call__(self, events, sensor_size, ordering, images=None, multi_image=None):
         return functional.crop_numpy(
-            events,
-            self.images,
-            sensor_size,
-            ordering,
-            self.target_size,
-            self.multi_image,
-        )
-
-
-class FlipLR(object):
-    def __init__(self, images=None, flip_probability=0.5, multi_image=None):
-        self.images = images
-        self.flip_probability = flip_probability
-        self.multi_image = multi_image
-
-    def __call__(self, events, sensor_size, ordering):
-        return functional.flip_lr_numpy(
-            events,
-            self.images,
-            sensor_size,
-            ordering,
-            self.flip_probability,
-            self.multi_image,
-        )
-
-
-class FlipUD(object):
-    def __init__(self, images=None, flip_probability=0.5, multi_image=None):
-        self.images = images
-        self.flip_probability = flip_probability
-        self.multi_image = multi_image
-
-    def __call__(self, events, sensor_size, ordering):
-        return functional.flip_ud_numpy(
-            events,
-            self.images,
-            sensor_size,
-            ordering,
-            self.flip_probability,
-            self.multi_image,
+            events, images, sensor_size, ordering, self.target_size, multi_image
         )
 
 
@@ -90,13 +49,30 @@ class DropEvent(object):
         self.drop_probability = drop_probability
         self.random_drop_probability = random_drop_probability
 
-    def __call__(self, events, sensor_size, ordering):
-        return functional.drop_event_numpy(
-            events,
-            sensor_size,
-            ordering,
-            self.drop_probability,
-            self.random_drop_probability,
+    def __call__(self, events, sensor_size, ordering, images=None, multi_image=None):
+        events = functional.drop_event_numpy(
+            events, self.drop_probability, self.random_drop_probability
+        )
+        return events, images
+
+
+class FlipLR(object):
+    def __init__(self, flip_probability=0.5):
+        self.flip_probability = flip_probability
+
+    def __call__(self, events, sensor_size, ordering, images=None, multi_image=None):
+        return functional.flip_lr_numpy(
+            events, images, sensor_size, ordering, self.flip_probability, multi_image
+        )
+
+
+class FlipUD(object):
+    def __init__(self, flip_probability=0.5):
+        self.flip_probability = flip_probability
+
+    def __call__(self, events, sensor_size, ordering, images=None, multi_image=None):
+        return functional.flip_ud_numpy(
+            events, images, sensor_size, ordering, self.flip_probability, multi_image
         )
 
 
@@ -104,10 +80,11 @@ class RefractoryPeriod(object):
     def __init__(self, refractory_period=0.5):
         self.refractory_period = refractory_period
 
-    def __call__(self, events, sensor_size, ordering):
-        return functional.refractory_period_numpy(
+    def __call__(self, events, sensor_size, ordering, images=None, multi_image=None):
+        events = functional.refractory_period_numpy(
             events, sensor_size, ordering, self.refractory_period
         )
+        return events, images
 
 
 class SpatialJitter(object):
@@ -120,7 +97,7 @@ class SpatialJitter(object):
         events = functional.spatial_jitter_numpy(
             events, ordering, self.variance_x, self.variance_y, self.sigma_x_y
         )
-        return (events, images)
+        return events, images
 
 
 class SpatioTemporalTransform(object):
@@ -129,8 +106,8 @@ class SpatioTemporalTransform(object):
         self.temporal_transform = temporal_transform
         self.roll = roll
 
-    def __call__(self, events, sensor_size, ordering):
-        return functional.st_transform(
+    def __call__(self, events, sensor_size, ordering, images=None, multi_image=None):
+        events = functional.st_transform(
             events,
             sensor_size,
             ordering,
@@ -138,6 +115,7 @@ class SpatioTemporalTransform(object):
             self.temporal_transform,
             self.roll,
         )
+        return events, images
 
 
 class TimeJitter(object):
@@ -146,7 +124,7 @@ class TimeJitter(object):
 
     def __call__(self, events, sensor_size, ordering, images=None, multi_image=None):
         events = functional.time_jitter_numpy(events, ordering, self.variance)
-        return (events, images)
+        return events, images
 
 
 class TimeReversal(object):
@@ -164,7 +142,8 @@ class TimeSkew(object):
         self.coefficient = coefficient
         self.offset = offset
 
-    def __call__(self, events, sensor_size, ordering):
-        return functional.time_skew_numpy(
+    def __call__(self, events, sensor_size, ordering, images=None, multi_image=None):
+        events = functional.time_skew_numpy(
             events, ordering, self.coefficient, self.offset
         )
+        return events, images
