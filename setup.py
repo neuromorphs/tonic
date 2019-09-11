b00676f20913225d31661462994df223d218e24f
Gregor Lenz <mail@lenzgregor.com>
2019-09-12 00:49:41 +0200

    add tqdm as dep

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="spike_data_augmentation",
-    version="0.0.1",
+    version="0.0.2",
     author="The Neuromorphs of Telluride",
     author_email="event-data-augmentation@googlegroups.com",
     description="Spike manipulation and augmentation",
@@ -13,8 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/neuromorphs/spike-data-augmentation",
     packages=["spike_data_augmentation"],
-    setup_requires=["numpy"],
-    install_requires=["numpy"],
+    install_requires=["numpy", "tqdm"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPL-3.0",
