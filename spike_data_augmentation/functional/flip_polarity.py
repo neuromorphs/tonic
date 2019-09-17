9473298494e5c4b8cea3b7b132e9b7a9bca63b64
Gregor Lenz <mail@lenzgregor.com>
2019-09-17 14:43:06 +0200

    update docstrings for crop, drop_event and flip_pol

--- spike_data_augmentation/functional/flip_polarity.py
+++ spike_data_augmentation/functional/flip_polarity.py
@@ -4,23 +4,19 @@ from .utils import guess_event_ordering_numpy
 
 
 def flip_polarity_numpy(events, flip_probability=0.5, ordering=None):
-    """
-    Flips polarity of individual events with probability
-    flip_probability:
+    """Flips polarity of individual events with flip_probability.
 
-
-    Arguments:
-    - events - ndarray of shape [num_events, num_event_channels]
-    - flip_probability - probability of flipping individual event
+    Args:
+        events: ndarray of shape [num_events, num_event_channels]
+        flip_probability: probability of flipping individual event
                          polarities
-    - ordering - ordering of the event tuple inside of events, if None
+        ordering: ordering of the event tuple inside of events, if None
                  the system will take a guess through
                  guess_event_ordering_numpy. This function requires 'p'
                  to be in the ordering and requires that polarity is
                  encoded as -1 or 1
-
     Returns:
-    - events - returns every event with p' = -p at flip_probability
+        augmented events - returns every event with p' = -p at flip_probability
                or p' = p at 1 - flip_probability
     """
 
