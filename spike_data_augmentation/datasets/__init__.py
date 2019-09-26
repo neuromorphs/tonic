a8abc36bb350f68a310fc6d97415d4083f1e87f9
bamsumit <bam_sumit@hotmail.com>
2019-09-26 12:36:03 +0800

    added ibmgetsture in datasets.init script

--- spike_data_augmentation/datasets/__init__.py
+++ spike_data_augmentation/datasets/__init__.py
@@ -1,4 +1,5 @@
 from .nmnist import NMNIST
+from .ibmGesture import IBMGesture
 from .dataloader import Dataloader
 
-__all__ = ["NMNIST", "Dataloader"]
+__all__ = ["NMNIST", "IBMGesture", "Dataloader"]
