421f2986c7e3dedd99d132d7b8f11d29956cc8c4
Gregor Lenz <mail@lenzgregor.com>
2019-11-20 18:20:40 +0100

    add method mask_isolated

---
+++ spike_data_augmentation/functional/mask_isolated.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+from .utils import guess_event_ordering_numpy
+
+
+def mask_isolated_numpy(
+    events, sensor_size=(346, 260), ordering=None, filter_time=10000
+):
+    """Drops events that have no spatiotemporal neighbour within one pixel
+    and filter_time time units. Useful to filter noisy recordings.
+
+    Args:
+        events: ndarray of shape [num_events, num_event_channels]
+        sensor_size: size of the sensor that was used [W,H]
+        ordering: ordering of the event tuple inside of events, if None
+                  the system will take a guess through
+                  guess_event_ordering_numpy. This function requires 'x',
+                  'y' and 't' to be in the ordering
+        filter_time: maximum temporal distance to next event, otherwise dropped
+
+    Returns:
+        Noise filtered events.
+    """
+
+    if ordering is None:
+        ordering = guess_event_ordering_numpy(events)
+    assert "x" and "y" and "t" in ordering
+
+    x_index = ordering.find("x")
+    y_index = ordering.find("y")
+    t_index = ordering.find("t")
+
+    events_copy = np.zeros(events.shape, dtype=events.dtype)
+    copy_index = 0
+    width = int(sensor_size[0])
+    height = int(sensor_size[1])
+    timestamp_memory = np.zeros((width, height), dtype=events.dtype)
+
+    for event in events:
+        x = int(event[x_index])
+        y = int(event[y_index])
+        t = event[t_index]
+        timestamp_memory[x, y] = t + filter_time
+        if (
+            (x > 0 and timestamp_memory[x - 1, y] > t)
+            or (x < width - 1 and timestamp_memory[x + 1, y] > t)
+            or (y > 0 and timestamp_memory[x, y - 1] > t)
+            or (y < height - 1 and timestamp_memory[x, y + 1] > t)
+        ):
+            events_copy[copy_index] = event
+            copy_index += 1
+
+    return events_copy[:copy_index]
