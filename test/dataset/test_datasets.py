aaa511227d2c551e287d2800653a54911d67291e
Gregor Lenz <mail@lenzgregor.com>
2019-12-02 16:01:28 +0000

    correct assertions and skip testclass for now

--- test/dataset/test_datasets.py
+++ test/dataset/test_datasets.py
@@ -3,31 +3,32 @@ import numpy as np
 import spike_data_augmentation.datasets as datasets
 
 
+@unittest.skip("Super slow!")
 class TestDatasets(unittest.TestCase):
     def testIBMGesturetraining(self):
         trainingset = datasets.IBMGesture(save_to="./data", train=True)
         trainloader = datasets.Dataloader(trainingset, shuffle=False)
         events, label = next(iter(trainloader))
 
-        self.assertEqual(events.shape[0], 3000)
-        self.assertEqual(label, 0)
-        self.assertEqual(len(trainingset), 60000)
+        self.assertEqual(events.shape[0], 520335)
+        self.assertEqual(label, 8)
+        self.assertEqual(len(trainingset), 1077)
 
     def testIBMGesturetesting(self):
         testset = datasets.IBMGesture(save_to="./data", train=False)
         testloader = datasets.Dataloader(testset, shuffle=False)
         events, label = next(iter(testloader))
 
-        self.assertEqual(events.shape[0], 3000)
-        self.assertEqual(label, 0)
-        self.assertEqual(len(testset), 10000)
+        self.assertEqual(events.shape[0], 546808)
+        self.assertEqual(label, 8)
+        self.assertEqual(len(testset), 264)
 
     def testNMNISTtraining(self):
         trainingset = datasets.NMNIST(save_to="./data", train=True)
         trainloader = datasets.Dataloader(trainingset, shuffle=False)
         events, label = next(iter(trainloader))
 
-        self.assertEqual(events.shape[0], 3000)
+        self.assertEqual(events.shape[0], 4893)
         self.assertEqual(label, 0)
         self.assertEqual(len(trainingset), 60000)
 
@@ -36,6 +37,6 @@ class TestDatasets(unittest.TestCase):
         testloader = datasets.Dataloader(testset, shuffle=False)
         events, label = next(iter(testloader))
 
-        self.assertEqual(events.shape[0], 3000)
+        self.assertEqual(events.shape[0], 5412)
         self.assertEqual(label, 0)
         self.assertEqual(len(testset), 10000)
