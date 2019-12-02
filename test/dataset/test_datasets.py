6ab591ee4f3c688a9101312ea97d16a81d6af339
Gregor Lenz <mail@lenzgregor.com>
2019-12-02 15:39:42 +0000

    add IBMGesture data set tests

--- test/dataset/test_datasets.py
+++ test/dataset/test_datasets.py
@@ -4,11 +4,30 @@ import spike_data_augmentation.datasets as datasets
 
 
 class TestDatasets(unittest.TestCase):
+    def testIBMGesturetraining(self):
+        trainingset = datasets.IBMGesture(save_to="./data", train=True)
+        trainloader = datasets.Dataloader(trainingset, shuffle=False)
+        events, label = next(iter(trainloader))
+
+        self.assertEqual(events.shape[0], 3000)
+        self.assertEqual(label, 0)
+        self.assertEqual(len(trainingset), 60000)
+
+    def testIBMGesturetesting(self):
+        testset = datasets.IBMGesture(save_to="./data", train=False)
+        testloader = datasets.Dataloader(testset, shuffle=False)
+        events, label = next(iter(testloader))
+
+        self.assertEqual(events.shape[0], 3000)
+        self.assertEqual(label, 0)
+        self.assertEqual(len(testset), 10000)
+
     def testNMNISTtraining(self):
         trainingset = datasets.NMNIST(save_to="./data", train=True)
         trainloader = datasets.Dataloader(trainingset, shuffle=False)
         events, label = next(iter(trainloader))
 
+        self.assertEqual(events.shape[0], 3000)
         self.assertEqual(label, 0)
         self.assertEqual(len(trainingset), 60000)
 
@@ -17,5 +36,6 @@ class TestDatasets(unittest.TestCase):
         testloader = datasets.Dataloader(testset, shuffle=False)
         events, label = next(iter(testloader))
 
+        self.assertEqual(events.shape[0], 3000)
         self.assertEqual(label, 0)
-        self.assertEqual(len(testset, 10000))
+        self.assertEqual(len(testset), 10000)
