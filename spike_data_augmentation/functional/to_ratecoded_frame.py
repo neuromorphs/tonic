0c5e5e292e7d9a63bb920bcbcb697dc33e8efdc3
Gregor Lenz <mail@lenzgregor.com>
2019-12-06 16:30:02 +0000

    remove sparse package dep

--- spike_data_augmentation/functional/to_ratecoded_frame.py
+++ spike_data_augmentation/functional/to_ratecoded_frame.py
@@ -1,18 +1,15 @@
 import numpy as np
-import sparse
+import math
 
 
 def to_ratecoded_frame_numpy(events, sensor_size, ordering, frame_time=5000):
     """Representation that creates frames by encoding the rate of events.
 
     Args:
-        surface_dimensions (int, int): width does not have to be equal to height, however both numbers have to be odd.
-        tau (float): time constant to decay events around occuring event with.
-        decay (str): can be either 'lin' or 'exp', corresponding to linear or exponential decay.
-        merge_polarities (bool): flag that tells whether polarities should be taken into account separately or not.
+        frame_time: time value that events should be binned into
 
     Returns:
-        array of timesurfaces with dimensions (w,h)
+        n rate-coded frames (n,w,h) 
     """
     assert "x" and "y" and "t" and "p" in ordering
     assert len(sensor_size) == 2
@@ -23,29 +20,18 @@ def to_ratecoded_frame_numpy(events, sensor_size, ordering, frame_time=5000):
     n_events = len(events)
 
     events[:, p_index][events[:, p_index] == -1] = 0
+    n_bins = math.ceil(events[-1, t_index] / frame_time)
 
-    frame_index_borders = []
-    n = 1
-    for i, t in enumerate(events[:, t_index]):
-        if t >= frame_time * n:
-            frame_index_borders.append(i - 1)
+    n = 0
+    frames = np.zeros((n_bins + 2,) + sensor_size)
+    for i, e in enumerate(events):
+        x = int(e[x_index])
+        y = int(e[y_index])
+        t = e[t_index]
+        p = e[p_index]
+
+        if t >= frame_time * (n + 1):
             n += 1
-    frame_events = np.split(events, frame_index_borders)
-
-    frames = np.zeros((len(frame_events) + 2,) + sensor_size)
-    f = 0
-    for e in frame_events:
-        sparse_events = sparse.COO(
-            (
-                e[:, p_index],
-                (
-                    e[:, t_index].astype(int),
-                    e[:, x_index].astype(int),
-                    e[:, y_index].astype(int),
-                ),
-            )
-        ).todense()
-        frames[f, :, :] = np.sum(sparse_events, axis=0)
-        f += 1
+        frames[n, x, y] += p
 
     return frames
