fca475cf56ba9714b54ddfdf14cf0d5de197155e
Gregor Lenz <mail@lenzgregor.com>
2019-12-09 01:16:38 +0100

    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="spike_data_augmentation",
-    version="0.0.2",
+    version="0.0.3",
     author="The Neuromorphs of Telluride",
     author_email="event-data-augmentation@googlegroups.com",
     description="Spike manipulation and augmentation",
