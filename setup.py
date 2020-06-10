93853fbe7c66657bfdb56f5dec12bd45a7ab1096
Gregor Lenz <mail@lenzgregor.com>
2020-06-10 19:34:53 +0100

    modified setup classifier

--- setup.py
+++ setup.py
@@ -24,6 +24,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-        "Framework :: Tonic",
+        "Intended Audience :: Science/Research",
     ],
 )
