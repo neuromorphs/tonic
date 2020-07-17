8ad703d45e263be5a9923bd51b4d1bf822826e35
Gregor Lenz <mail@lenzgregor.com>
2020-07-18 00:33:34 +0200

    parameterize dataset tests

--- test/dataset/test_datasets.py
+++ test/dataset/test_datasets.py
@@ -1,42 +1,34 @@
 import unittest
 import numpy as np
+from parameterized import parameterized
 import tonic.datasets as datasets
 
 
 #@unittest.skip("Super slow!")
 class TestDatasets(unittest.TestCase):
-    def testIBMGesturetraining(self):
-        trainingset = datasets.IBMGesture(save_to="./data", train=True)
-        trainloader = datasets.Dataloader(trainingset, shuffle=False)
-        events, label = next(iter(trainloader))
+    @parameterized.expand([
+        (True, 440860, 7, 1077),
+        (False, 288123, 7, 264),
+    ])
+    def testIBMGesture(self, train, n_events, true_label, n_samples):
+        dataset = datasets.IBMGesture(save_to="./data", train=train)
+        dataloader = datasets.DataLoader(dataset, shuffle=False)
+        events, label = next(iter(dataloader))
 
-        self.assertEqual(events.shape[0], 440860)
-        self.assertEqual(label, 7)
-        self.assertEqual(len(trainingset), 1077)
+        self.assertEqual(events.shape[0], n_events)
+        self.assertEqual(label, true_label)
+        self.assertEqual(len(dataset), n_samples)
 
-    def testIBMGesturetesting(self):
-        testset = datasets.IBMGesture(save_to="./data", train=False)
-        testloader = datasets.Dataloader(testset, shuffle=False)
-        events, label = next(iter(testloader))
 
-        self.assertEqual(events.shape[0], 288123)
-        self.assertEqual(label, 7)
-        self.assertEqual(len(testset), 264)
+    @parameterized.expand([
+        (True, 4733, 0, 60000),
+        (False, 5100, 0, 10000),
+    ])
+    def testNMNIST(self, train, n_events, true_label, n_samples):
+        dataset = datasets.NMNIST(save_to="./data", train=train)
+        dataloader = datasets.DataLoader(dataset, shuffle=False)
+        events, label = next(iter(dataloader))
 
-    def testNMNISTtraining(self):
-        trainingset = datasets.NMNIST(save_to="./data", train=True)
-        trainloader = datasets.Dataloader(trainingset, shuffle=False)
-        events, label = next(iter(trainloader))
-
-        self.assertEqual(events.shape[0], 4733)
-        self.assertEqual(label, 0)
-        self.assertEqual(len(trainingset), 60000)
-
-    def testNMNISTtesting(self):
-        testset = datasets.NMNIST(save_to="./data", train=False)
-        testloader = datasets.Dataloader(testset, shuffle=False)
-        events, label = next(iter(testloader))
-
-        self.assertEqual(events.shape[0], 5100)
-        self.assertEqual(label, 0)
-        self.assertEqual(len(testset), 10000)
+        self.assertEqual(events.shape[0], n_events)
+        self.assertEqual(label, true_label)
+        self.assertEqual(len(dataset), n_samples)
