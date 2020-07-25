a5da89c3fb9e765aa8c92a4371c579d990470c3f
Gregor Lenz <mail@lenzgregor.com>
2020-07-25 20:37:46 +0100

    deactivate dataset tests for now

--- test/dataset/test_datasets.py
+++ test/dataset/test_datasets.py
@@ -4,13 +4,11 @@ from parameterized import parameterized
 import tonic.datasets as datasets
 
 
-# @unittest.skip("Super slow!")
+@unittest.skip("Super slow!")
 class TestDatasets(unittest.TestCase):
-    download = False
+    download = True
 
-    @parameterized.expand(
-        [(True, 440860, 7, 1077), (False, 288123, 7, 264),]
-    )
+    @parameterized.expand([(True, 440860, 7, 1077), (False, 288123, 7, 264)])
     def testIBMGesture(self, train, n_events, true_label, n_samples):
         dataset = datasets.IBMGesture(
             save_to="./data", train=train, download=self.download
@@ -22,9 +20,7 @@ class TestDatasets(unittest.TestCase):
         self.assertEqual(label, true_label)
         self.assertEqual(len(dataset), n_samples)
 
-    @parameterized.expand(
-        [(True, 4733, 0, 60000), (False, 5100, 0, 10000),]
-    )
+    @parameterized.expand([(True, 4733, 0, 60000), (False, 5100, 0, 10000)])
     def testNMNIST(self, train, n_events, true_label, n_samples):
         dataset = datasets.NMNIST(save_to="./data", train=train, download=self.download)
         dataloader = datasets.DataLoader(dataset, batch_size=None, shuffle=False)
