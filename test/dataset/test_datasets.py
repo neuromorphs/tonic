33211f9ec11bc039189f29d2f309c8196030b600
Gregor Lenz <mail@lenzgregor.com>
2020-07-28 22:20:17 +0200

    add NMNIST for first_saccade_only

--- test/dataset/test_datasets.py
+++ test/dataset/test_datasets.py
@@ -6,7 +6,7 @@ import tonic.datasets as datasets
 
 @unittest.skip("Super slow!")
 class TestDatasets(unittest.TestCase):
-    download = True
+    download = False
 
     @parameterized.expand([(True, 440860, 7, 1077), (False, 288123, 7, 264)])
     def testIBMGesture(self, train, n_events, true_label, n_samples):
@@ -20,9 +20,20 @@ class TestDatasets(unittest.TestCase):
         self.assertEqual(label, true_label)
         self.assertEqual(len(dataset), n_samples)
 
-    @parameterized.expand([(True, 4733, 0, 60000), (False, 5100, 0, 10000)])
-    def testNMNIST(self, train, n_events, true_label, n_samples):
-        dataset = datasets.NMNIST(save_to="./data", train=train, download=self.download)
+    @parameterized.expand(
+        [
+            (True, False, 4733, 0, 60000),
+            (False, False, 5100, 0, 10000),
+            (False, True, 1706, 0, 10000),
+        ]
+    )
+    def testNMNIST(self, train, first_saccade_only, n_events, true_label, n_samples):
+        dataset = datasets.NMNIST(
+            save_to="./data",
+            train=train,
+            download=self.download,
+            first_saccade_only=first_saccade_only,
+        )
         dataloader = datasets.DataLoader(dataset, batch_size=None, shuffle=False)
         events, label = next(iter(dataloader))
 
