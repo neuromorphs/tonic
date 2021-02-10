42535db7a4a9e07061148d9a13005ab3cdec3917
Gregor Lenz <mail@lenzgregor.com>
2021-02-10 16:53:06 +0100

    add a few newlines

--- tonic/transforms.py
+++ tonic/transforms.py
@@ -3,8 +3,10 @@ from . import functional
 
 class Compose:
     """Composes several transforms together.
+    
     Args:
         transforms (list of ``Transform`` objects): list of transform(s) to compose. Even when using a single transform, the Compose wrapper is necessary.
+    
     Example:
         >>> transforms.Compose([
         >>>     transforms.Denoise(),
