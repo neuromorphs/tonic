36fd34454fdb8fde4d54ba44a5c487d151e1d1e4
AntoineGrimaldi <antoine.grimaldi@univ-amu.fr>
2021-05-11 14:59:44 +0200

    change variance for std

--- tonic/functional/time_jitter.py
+++ tonic/functional/time_jitter.py
@@ -4,13 +4,13 @@ from .utils import guess_event_ordering_numpy
 
 
 def time_jitter_numpy(
-    events, ordering=None, variance=1, integer_timestamps=False, clip_negative=True
+    events, ordering=None, std=1, integer_timestamps=False, clip_negative=True
 ):
     """Changes timestamp for each event by drawing samples from a
     Gaussian distribution with the following properties:
 
         mean = [t]
-        variance = variance
+        std = standard deviation
 
     Will clip negative timestamps by default.
 
@@ -19,7 +19,7 @@ def time_jitter_numpy(
         ordering: ordering of the event tuple inside of events, if None
                   the system will take a guess. This function requires 't'
                   to be in the ordering
-        variance: change the variance of the time jitter
+        std: change the standard deviation of the time jitter
         integer_timestamps: will round the jitter that is added to timestamps
         clip_negative: drops events that have negative timestamps, otherwise set to zero.
 
@@ -32,7 +32,7 @@ def time_jitter_numpy(
     assert "t" in ordering
 
     t_index = ordering.find("t")
-    shifts = np.random.normal(0, variance, len(events))
+    shifts = np.random.normal(0, std, len(events))
 
     if integer_timestamps:
         shifts = shifts.round()
