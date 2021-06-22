68cbbcb60e447058e968e33fe670784a888a3eda
Gregor Lenz <mail@lenzgregor.com>
2021-06-22 17:33:03 +0200

    split random input event generator function into vision and audio dataset events

--- test/functional/utils.py
+++ test/functional/utils.py
@@ -12,25 +12,36 @@ def create_random_input_with_ordering(ordering, sensor_size=(200, 100), datatype
     - ordering as input
     - multi_image - True
     """
+    assert "x" and "t" and "p" in ordering
     x_index = ordering.find("x")
-    y_index = ordering.find("y")
     t_index = ordering.find("t")
     p_index = ordering.find("p")
 
-    events = np.random.rand(10000, 4)
-
-    events[:, x_index] = (events[:, x_index] * sensor_size[0]).astype(int)
-    events[:, y_index] = (events[:, y_index] * sensor_size[1]).astype(int)
-    events[events[:, p_index] < 0.5, p_index] = -1
-    events[events[:, p_index] >= 0.5, p_index] = 1
+    # vision sensor
+    if "y" in ordering:
+        y_index = ordering.find("y")
+        assert len(sensor_size) == 2
+        events = np.random.rand(10000, 4)
+        events[:, x_index] = (events[:, x_index] * sensor_size[0]).astype(int)
+        events[:, y_index] = (events[:, y_index] * sensor_size[1]).astype(int)
+        events[events[:, p_index] < 0.5, p_index] = -1
+        events[events[:, p_index] >= 0.5, p_index] = 1
+        images = np.random.rand(4, sensor_size[1], sensor_size[0])
+        is_multi_image = True
+    # audio sensor
+    else:
+        sensor_size = (100,)
+        events = np.random.rand(10000, 3)
+        events[:, x_index] = (events[:, x_index] * sensor_size[0]).astype(int)
+        events[:, p_index] = 1
+        images = None
+        is_multi_image = None
 
     # sort timestamps to ensure the times are sequential
     events[:, t_index] = np.sort(events[:, t_index]) * 10000
 
     if datatype != None:
         events = events.astype(datatype)
-    images = np.random.rand(4, sensor_size[1], sensor_size[0])
-
-    is_multi_image = True
-
+        
+    
     return events, images, sensor_size, is_multi_image
