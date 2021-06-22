710dabcec393c15b9ffe66bfd922d3b3c3aa496e
Gregor Lenz <mail@lenzgregor.com>
2021-06-22 17:33:29 +0200

    add sparse tensor support for audio datasets

--- test/functional/test_torch.py
+++ test/functional/test_torch.py
@@ -15,7 +15,7 @@ class TestFunctionalTorch(unittest.TestCase):
         p_index = ordering.find("p")
         return x_index, y_index, t_index, p_index
 
-    @parameterized.expand([("xytp", True), ("typx", False)])
+    @parameterized.expand([("xytp", True), ("typx", False), ("txp", True), ("xtp", False),])
     def testToSparseTensor(self, ordering, merge_polarities):
         (
             events,
@@ -33,10 +33,12 @@ class TestFunctionalTorch(unittest.TestCase):
         
         self.assertTrue(isinstance(tensor, torch.Tensor))
         self.assertTrue(tensor.is_sparse)
-        self.assertEqual(len(tensor.shape), 4, "Tensor should have 4 dimensions, (TCXY)")
+        self.assertEqual(len(tensor.shape), len(ordering), "Tensor does not have the right dimensions, should be equivalent to length of ordering (TCXY) or (TCX)")
         self.assertEqual(events.shape[0], tensor.coalesce().values().sum(), "Sparse tensor values should contain as many 1s as there are events.")
         self.assertEqual(tensor.size()[2:], sensor_size, "Sparse tensor should have sensor size dimensions.")
+        self.assertGreater(tensor.size()[0], 10, "There are probably more than 10 timestamps in the original events. This error normally occurs when converting float timestamp representations to integer indices for sparse tensors.")
         if merge_polarities:
             self.assertEqual(tensor.shape[1], 1)
         else:
-            self.assertEqual(tensor.shape[1], 2)
+            self.assertEqual(tensor.shape[1], len(np.unique(events[:,p_index])), "Amount of channels is not equivalent to unique number of polarities in events")
+        
--- tonic/functional/to_sparse_tensor.py
+++ tonic/functional/to_sparse_tensor.py
@@ -8,39 +8,45 @@ def to_sparse_tensor_pytorch(events, sensor_size, ordering, merge_polarities=Fal
     of 1 for each index, which signify a spike. The shape of the tensor will be (TCWH).
 
     Args:
-        merge_polarities (bool): flag that decides whether to combine positive and negative polarities into
-                                a single channel or split them in two. If True, the number of channels for
-                                indices is 1, otherwise it's 2. Regardless of this flag, all values assigned
-                                to indices will be 1, which signify a spike.
+        merge_polarities (bool): flag that decides whether to combine polarities into a single channel
+                                or split them into separate channels. If True, the number of channels for
+                                indices is 1, otherwise it's the number of different polarities. Regardless 
+                                of this flag, all values assigned to indices will be 1, which signify a spike.
 
     Returns:
         sparse tensor in TxCxWxH format, where T is timesteps, C is the number of channels for each polarity,
         and W and H are always the size of the sensor.
     """
-    assert "x" and "y" and "t" and "p" in ordering
+    assert "x" and "t" and "p" in ordering
     x_index = ordering.find("x")
-    y_index = ordering.find("y")
     t_index = ordering.find("t")
     p_index = ordering.find("p")
 
     if len(events.shape) != 2:
         raise RuntimeError(
-            "Will only convert to sparse tensor from (N,E) (i.e., a list of events)"
-            " dimension."
+            "Will only convert to sparse tensor from array of shape (N,E)."
         )
 
     # in any case, all the values in the sparse tensor will be 1, signifying a spike
     values = torch.ones(events.shape[0])
 
+    # prevents polarities used as indices that are not 0
+    if len(np.unique(events[:, p_index])) == 1: merge_polarities = True
+        
     if merge_polarities:  # the indices need to start at 0
         events[:, p_index] = 0
         n_channels = 1
     else:  # avoid any negative indices
         events[events[:, p_index] == -1, p_index] = 0
-        n_channels = 2
+        n_channels = len(np.unique(events[:, p_index]))
 
     max_time = int(max(events[:, t_index]) + 1)
-    indices = torch.LongTensor(events[:, [t_index, p_index, x_index, y_index]]).T
+    
+    if "y" in ordering:   
+        y_index = ordering.find("y")
+        indices = torch.LongTensor(events[:, [t_index, p_index, x_index, y_index]]).T
+    else:
+        indices = torch.LongTensor(events[:, [t_index, p_index, x_index]]).T
 
     return torch.sparse.FloatTensor(
         indices, values, torch.Size([max_time, n_channels, *sensor_size])
