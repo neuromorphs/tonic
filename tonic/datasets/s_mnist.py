bfc6fcef0f28f9de444de7983aa3c4fa6084e95f
neworderofjamie <neworderofjamie@gmail.com>
2021-07-09 15:53:49 +0100

    add classes

--- tonic/datasets/s_mnist.py
+++ tonic/datasets/s_mnist.py
@@ -17,7 +17,19 @@ class SMNIST(Dataset):
     test_labels_file = "t10k-labels-idx1-ubyte"
     sensor_size = (99,)
     ordering = "txp"
-    
+
+    classes = [
+        "0 - zero",
+        "1 - one",
+        "2 - two",
+        "3 - three",
+        "4 - four",
+        "5 - five",
+        "6 - six",
+        "7 - seven",
+        "8 - eight",
+        "9 - nine"]
+
     def __init__(self, save_to, train=True, duplicate=True, dt=1000.0, 
                  download=True, transform=None, target_transform=None):
         super(SMNIST, self).__init__(save_to, transform=transform, 
