a6ad57364c6fc8d060278fa9e98094aca589507c
Gregor Lenz <mail@lenzgregor.com>
2021-07-19 09:04:08 +0200

    refactor dataset tests not to use parameterized but purely pytest instead

--- test/dataset/test_datasets.py
+++ test/dataset/test_datasets.py
@@ -1,30 +1,34 @@
 import unittest
 import numpy as np
-from parameterized import parameterized
+import pytest
 import tonic.datasets as datasets
 
 
-@unittest.skip("Super slow!")
-class TestDatasets(unittest.TestCase):
+@pytest.mark.skip(reason="Dataset tests are super slow!")
+class TestDatasets:
     download = False
 
-    @parameterized.expand([(True, 927713, 3, 1077), (False, 944776, 3, 264)])
+    @pytest.mark.parametrize(
+        "train, n_events, true_label, n_samples",
+        [(True, 927713, 3, 1077), (False, 944776, 3, 264)],
+    )
     def testDVSGesture(self, train, n_events, true_label, n_samples):
         dataset = datasets.DVSGesture(
             save_to="./data", train=train, download=self.download
         )
         events, label = dataset[0]
 
-        self.assertEqual(events.shape[0], n_events)
-        self.assertEqual(label, true_label)
-        self.assertEqual(len(dataset), n_samples)
+        assert events.shape[0] == n_events
+        assert label == true_label
+        assert len(dataset) == n_samples
 
-    @parameterized.expand(
+    @pytest.mark.parametrize(
+        "train, first_saccade_only, n_events, true_label, n_samples",
         [
             (True, False, 5520, 3, 60000),
             (False, False, 6144, 3, 10000),
             (False, True, 2126, 3, 10000),
-        ]
+        ],
     )
     def testNMNIST(self, train, first_saccade_only, n_events, true_label, n_samples):
         dataset = datasets.NMNIST(
@@ -35,85 +39,101 @@ class TestDatasets(unittest.TestCase):
         )
         events, label = dataset[0]
 
-        self.assertEqual(events.shape[0], n_events)
-        self.assertEqual(label, true_label)
-        self.assertEqual(len(dataset), n_samples)
+        assert events.shape[0] == n_events
+        assert label == true_label
+        assert len(dataset) == n_samples
 
-    @parameterized.expand([(True, 1309, 0, 15422), (False, 2308, 0, 8607)])
+    @pytest.mark.parametrize(
+        "train, n_events, true_label, n_samples",
+        [(True, 1309, 0, 15422), (False, 2308, 0, 8607)],
+    )
     def testNCARS(self, train, n_events, true_label, n_samples):
         dataset = datasets.NCARS(save_to="./data", train=train, download=self.download)
         events, label = dataset[0]
 
-        self.assertEqual(events.shape[0], n_events)
-        self.assertEqual(label, true_label)
-        self.assertEqual(len(dataset), n_samples)
+        assert events.shape[0] == n_events
+        assert label == true_label
+        assert len(dataset) == n_samples
 
-    @parameterized.expand([(True, 4819, 3, 48), (False, 5366, 3, 20)])
+    @pytest.mark.parametrize(
+        "train, n_events, true_label, n_samples",
+        [(True, 4819, 3, 48), (False, 5366, 3, 20)],
+    )
     def testPOKERDVS(self, train, n_events, true_label, n_samples):
         dataset = datasets.POKERDVS(
             save_to="./data", train=train, download=self.download
         )
         events, label = dataset[0]
 
-        self.assertEqual(events.shape[0], n_events)
-        self.assertEqual(label, true_label)
-        self.assertEqual(len(dataset), n_samples)
+        assert events.shape[0] == n_events
+        assert label == true_label
+        assert len(dataset) == n_samples
 
-    @parameterized.expand([(88007, "BACKGROUND_Google", 8709)])
+    @pytest.mark.parametrize(
+        "n_events, true_label, n_samples", [(88007, "BACKGROUND_Google", 8709)]
+    )
     def testNCALTECH101(self, n_events, true_label, n_samples):
         dataset = datasets.NCALTECH101(save_to="./data", download=self.download)
         events, label = dataset[0]
 
-        self.assertEqual(events.shape[0], n_events)
-        self.assertEqual(label, true_label)
-        self.assertEqual(len(dataset), n_samples)
+        assert events.shape[0] == n_events
+        assert label == true_label
+        assert len(dataset) == n_samples
 
-    @parameterized.expand([(915556, 0, 304, True), (227321, 0, 1342, False)])
+    @pytest.mark.parametrize(
+        "n_events, true_label, n_samples, walk_subset",
+        [(915556, 0, 304, True), (227321, 0, 1342, False)],
+    )
     def testNavGestures(self, n_events, true_label, n_samples, walk_subset):
         dataset = datasets.NavGesture(
             save_to="./data", walk_subset=walk_subset, download=self.download
         )
         events, label = dataset[0]
 
-        self.assertEqual(events.shape[0], n_events)
-        self.assertEqual(label, true_label)
-        self.assertEqual(len(dataset), n_samples)
+        assert events.shape[0] == n_events
+        assert label == true_label
+        assert len(dataset) == n_samples
 
-    @parameterized.expand([(15951, 0, 100800)])
+    @pytest.mark.parametrize("n_events, true_label, n_samples", [(15951, 0, 100800)])
     def testASLDVS(self, n_events, true_label, n_samples):
         dataset = datasets.ASLDVS(save_to="./data", download=self.download)
         events, label = dataset[0]
 
-        self.assertEqual(events.shape[0], n_events)
-        self.assertEqual(label, true_label)
-        self.assertEqual(len(dataset), n_samples)
+        assert events.shape[0] == n_events
+        assert label == true_label
+        assert len(dataset) == n_samples
 
-    @parameterized.expand([(True, 4278, 11, 8156), (False, 11273, 10, 2264)])
+    @pytest.mark.parametrize(
+        "train, n_events, true_label, n_samples",
+        [(True, 4278, 11, 8156), (False, 11273, 10, 2264)],
+    )
     def testSHD(self, train, n_events, true_label, n_samples):
         dataset = datasets.SHD(save_to="./data", train=train, download=self.download)
         events, label = dataset[0]
 
-        self.assertEqual(events.shape[0], n_events)
-        self.assertEqual(label, true_label)
-        self.assertEqual(len(dataset), n_samples)
+        assert events.shape[0] == n_events
+        assert label == true_label
+        assert len(dataset) == n_samples
 
-    @parameterized.expand(
+    @pytest.mark.parametrize(
+        "split, n_events, true_label, n_samples",
         [
             ("train", 12618, 0, 75466),
             ("test", 11671, 0, 20382),
             ("valid", 14591, 0, 9981),
-        ]
+        ],
     )
     def testSSC(self, split, n_events, true_label, n_samples):
         dataset = datasets.SSC(save_to="./data", split=split, download=self.download)
         events, label = dataset[0]
 
-        self.assertEqual(events.shape[0], n_events)
-        self.assertEqual(label, true_label)
-        self.assertEqual(len(dataset), n_samples)
+        assert events.shape[0] == n_events
+        assert label == true_label
+        assert len(dataset) == n_samples
 
-    @parameterized.expand(
-        [(True, 898, "man-jr-b-6", 8621), (False, 1257, "man-im-b-6", 8697),]
+    @pytest.mark.parametrize(
+        "train, n_events, true_label, n_samples",
+        [(True, 898, "man-jr-b-6", 8621), (False, 1257, "man-im-b-6", 8697),],
     )
     def testNTIDIGITS(self, train, n_events, true_label, n_samples):
         dataset = datasets.NTIDIGITS(
@@ -121,12 +141,13 @@ class TestDatasets(unittest.TestCase):
         )
         events, label = dataset[0]
 
-        self.assertEqual(events.shape[0], n_events)
-        self.assertEqual(label, true_label)
-        self.assertEqual(len(dataset), n_samples)
+        assert events.shape[0] == n_events
+        assert label == true_label
+        assert len(dataset) == n_samples
 
-    @parameterized.expand(
-        [("shapes_6dof", 17962477, 1356, 1),]
+    @pytest.mark.parametrize(
+        "recording, n_events, n_images, n_samples",
+        [("shapes_6dof", 17962477, 1356, 1),],
     )
     def testDAVISDATA(self, recording, n_events, n_images, n_samples):
         dataset = datasets.DAVISDATA(
@@ -134,6 +155,6 @@ class TestDatasets(unittest.TestCase):
         )
         events, imu, images, ground_truth = dataset[0]
 
-        self.assertEqual(events.shape[0], n_events)
-        self.assertEqual(images["frames"].shape[0], n_images)
-        self.assertEqual(len(dataset), n_samples)
+        assert events.shape[0] == n_events
+        assert images["frames"].shape[0] == n_images
+        assert len(dataset) == n_samples
