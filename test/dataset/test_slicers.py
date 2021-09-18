43a6b36e338b6b492da5c9f536111f349a0f82ba
Sadique Sheik <sadique.sheik@aictx.ai>
2021-09-18 06:56:19 +0200

    dummy tests added. require implementation

---
+++ test/dataset/test_slicers.py
@@ -0,0 +1,14 @@
+def test_slice_at_indices_class():
+    raise NotImplementedError
+
+
+def test_slice_at_time_points_class():
+    raise NotImplementedError
+
+
+def test_slice_by_event_count():
+    raise NotImplementedError
+
+
+def test_slice_by_time():
+    raise NotImplementedError
