cdf3f7e48077411d00ffac5451789dc9208e9434
Gregor Lenz <mail@lenzgregor.com>
2021-10-17 22:06:21 +0200

    add GPS data to VPR dataset

--- tonic/datasets/visual_place_recognition.py
+++ tonic/datasets/visual_place_recognition.py
@@ -28,12 +28,31 @@ class VPR(Dataset):
 
     base_url = "https://zenodo.org/record/4302805/files/"
     recordings = [  # recording names and their md5 hash
-        ["dvs_vpr_2020-04-21-17-03-03.bag", "04473f623aec6bda3d7eadfecfc1b2ce"],
-        ["dvs_vpr_2020-04-22-17-24-21.bag", "ca6db080a4054196fe65825bce3db351"],
-        ["dvs_vpr_2020-04-24-15-12-03.bag", "909569732e323ff04c94379a787f2a69"],
-        ["dvs_vpr_2020-04-27-18-13-29.bag", "e80b6c0434690908d855445792d4de3b"],
-        ["dvs_vpr_2020-04-28-09-14-11.bag", "7854ede61c0947adb0f072a041dc3bad"],
-        ["dvs_vpr_2020-04-29-06-20-23.bag", "d7ccfeb6539f1e7b077ab4fe6f45193c"],
+        [
+            ["dvs_vpr_2020-04-21-17-03-03.bag", "04473f623aec6bda3d7eadfecfc1b2ce"],
+            ["20200421_170039-sunset1_concat.nmea", "11f0107a4df845fd315e9134fbee5c1e"]
+        ],
+        [
+            ["dvs_vpr_2020-04-22-17-24-21.bag", "ca6db080a4054196fe65825bce3db351"],
+            ["20200422_172431-sunset2_concat.nmea", "ff879bf22a9552a6d8500a98cff6c7f9"]
+        ],
+        [
+            ["dvs_vpr_2020-04-24-15-12-03.bag", "909569732e323ff04c94379a787f2a69"],
+            ["20200424_151015-daytime_concat.nmea", "867fdf43ef393ac7e8de251c1a5cd585"]
+        ],
+        [
+            ["dvs_vpr_2020-04-27-18-13-29.bag", "e80b6c0434690908d855445792d4de3b"],
+            ["20200427_181204-night_concat.nmea", "441e6673e0dfc8746f76cd646c4aba8d"]
+        ],
+        [
+            ["dvs_vpr_2020-04-28-09-14-11.bag", "7854ede61c0947adb0f072a041dc3bad"],
+            ["20200428_091154-morning_concat.nmea", "b86af464ceac478711e52ef4271c198c"]
+        ],
+        [
+            ["dvs_vpr_2020-04-29-06-20-23.bag", "d7ccfeb6539f1e7b077ab4fe6f45193c"],
+            ["20200429_061912-sunrise_concat.nmea", "ec04cf35c10eb5b519b11297adef024b"]
+        ],
+        
     ]
 
     sensor_size = (346, 260, 2)
@@ -42,16 +61,17 @@ class VPR(Dataset):
 
     def __init__(self, save_to, transform=None):
         super(VPR, self).__init__(save_to, transform=transform)
-
+            
         if not self._check_exists():
             self.download()
 
     def __getitem__(self, index):
         """
         Returns:
-            a tuple of (data, None) where data is another tuple of (events, imu, images).
+            a tuple of (data, target) where data is another tuple of (events, imu, images) and target is gps positional data.
         """
-        file_path = os.path.join(self.location_on_system, self.recordings[index][0])
+        (bag_filename,_), (gps_filename,_) = self.recordings[index]
+        file_path = os.path.join(self.location_on_system, bag_filename)
         topics = importRosbag(filePathOrName=file_path, log="ERROR")
         events = topics["/dvs/events"]
         events["ts"] -= events["ts"][0]
@@ -65,23 +85,53 @@ class VPR(Dataset):
 #         images["frames"] = np.stack(images["frames"]) # errors for some recordings
         data = events, imu, images
 
+        targets = self.read_gps_file(os.path.join(self.location_on_system, gps_filename))
+        
         if self.transform is not None:
             data = self.transform(data)
-        return data, None
+        if self.target_transform is not None:
+            targets = self.target_transform(targets)
+        return data, targets
 
     def __len__(self):
         return len(self.recordings)
 
     def download(self):
-        for (recording, md5_hash) in self.recordings:
-            download_url(
-                self.base_url + recording,
-                self.location_on_system,
-                filename=recording,
-                md5=md5_hash,
-            )
+        for recording in self.recordings:
+            for filename, md5_hash in recording:
+                download_url(
+                    self.base_url + filename,
+                    self.location_on_system,
+                    filename=filename,
+                    md5=md5_hash,
+                )
 
     def _check_exists(self):
         # check if all filenames are correct
-        files_present = list([check_integrity(os.path.join(self.location_on_system, recording)) for recording, md5 in self.recordings])
+        files_present = list([check_integrity(os.path.join(self.location_on_system, filename)) for recording in self.recordings for filename, md5 in recording])
         return all(files_present)
+    
+    # code taken from https://github.com/Tobias-Fischer/ensemble-event-vpr/blob/master/read_gps.py
+    def read_gps_file(self, nmea_file_path):
+        import pynmea2
+        nmea_file = open(nmea_file_path, encoding='utf-8')
+        latitudes, longitudes, timestamps = [], [], []
+        first_timestamp = None
+        previous_lat, previous_lon = 0, 0
+
+        for line in nmea_file.readlines():
+            try:
+                msg = pynmea2.parse(line)
+                if first_timestamp is None:
+                    first_timestamp = msg.timestamp
+                if msg.sentence_type not in ['GSV', 'VTG', 'GSA']:
+                    dist_to_prev = np.linalg.norm(np.array([msg.latitude, msg.longitude]) - np.array([previous_lat, previous_lon]))
+                    if msg.latitude != 0 and msg.longitude != 0 and msg.latitude != previous_lat and msg.longitude != previous_lon and dist_to_prev > 0.0001:
+                        timestamp_diff = (msg.timestamp.hour - first_timestamp.hour) * 3600 + (msg.timestamp.minute - first_timestamp.minute) * 60 + (msg.timestamp.second - first_timestamp.second)
+                        latitudes.append(msg.latitude); longitudes.append(msg.longitude); timestamps.append(timestamp_diff)
+                        previous_lat, previous_lon = msg.latitude, msg.longitude
+
+            except pynmea2.ParseError as e:
+                continue
+
+        return np.array(np.vstack((latitudes, longitudes, timestamps))).T
