91378f2e5acf8826ac7c04c6d6925103aeed7331
Gregor Lenz <mail@lenzgregor.com>
2021-10-20 13:25:42 +0200

    update SlicedDataset to contain filepath for metadata

--- tonic/sliced_dataset.py
+++ tonic/sliced_dataset.py
@@ -1,44 +1,81 @@
+import os
+import h5py
 from dataclasses import dataclass
 from typing import Iterable, List, Any, Optional, Callable
 
 from .slicers import Slicer
 
 
+def save_metadata(path, metadata):
+    os.makedirs(path, exist_ok=True)
+    with h5py.File(os.path.join(path, 'slice_metadata.h5'), "w") as f:
+        f.create_dataset(f"metadata", data=metadata)
+    print("Metadata written to disk.")
+
+
+def load_metadata(path):
+    with h5py.File(os.path.join(path, 'slice_metadata.h5'), "r") as f:
+        metadata = f["metadata"][()]
+    return metadata
+    print("Read metadata from disk.")
+
+
 @dataclass
 class SlicedDataset:
     """The primary use case for a SlicedDataset is to cut existing examples in a dataset
-    into smaller chunks. For that it takes a regular dataset and a slicing method as input.
+    into smaller chunks. For that it takes an iterable dataset and a slicing method as input.
     It then generates metadata about the slices and where to find them in each original sample.
-    The new dataset length will be the sum of all slices for each sample.
+    The new dataset length will be the sum of all slices across samples.
 
     Parameters:
         dataset: a dataset object which implements __getitem__ and __len__ methods.
         slicer: a function which implements the tonic.slicers.Slicer protocol, meaning that
                 it doesn't have to inherit from it but implement all its methods.
+        metadata_path: filepath where slice metadata should be stored, so that it does not
+                       have to be recomputed the next time. If None, will be recomputed
+                       every time.
+        transform: Transforms to be applied on the data
+        target_transform: Transforms to be applied on the label/targets
     """
 
     dataset: Iterable
     slicer: Slicer
+    metadata_path: Optional[str] = None
     transform: Optional[Callable] = None
     target_transform: Optional[Callable] = None
 
     def __post_init__(self):
-        # Generate slicing metadata
-        self.metadata: List[Any] = []
-        self.slice_dataset_map = []
-        for dataset_index, (data, targets) in enumerate(self.dataset):
-            metadata = self.slicer.get_slice_metadata(data)
-            self.metadata.append(metadata)
-            self.slice_dataset_map += [
-                (dataset_index, slice_index) for slice_index in range(len(metadata))
-            ]
-
+        """
+        Will try to read metadata from disk to know where slices start and stop for each sample.
+        If no metadata_path is provided or no file slice_metadata.h5 is found in that path,
+        metadata will be generated from scratch.
+        """
+        if self.metadata_path:
+            try:
+                self.metadata = load_metadata(self.metadata_path)
+            except FileNotFoundError as _:
+                self.metadata = self.generate_metadata()
+                save_metadata(self.metadata_path, self.metadata)
+        else:
+            self.metadata = self.generate_metadata()
+        self.slice_dataset_map = [(sample_index, slice_index) 
+                                  for sample_index, sample_metadata in enumerate(self.metadata) 
+                                  for slice_index in range(len(sample_metadata))]
+        
+    def generate_metadata(self):
+        """
+        Slices every sample in the wrapped dataset and returns start and stop metadata
+        for each slice.
+        """
+        return [self.slicer.get_slice_metadata(data) for data, targets in self.dataset]
+        
     def __getitem__(self, item) -> Any:
         dataset_index, slice_index = self.slice_dataset_map[item]
         data, targets = self.dataset[dataset_index]
         data_slice = self.slicer.slice_with_metadata(
-            data, self.metadata[dataset_index]
-        )[slice_index]
+            data, self.metadata[dataset_index][slice_index]
+        )
+        # TODO: target slicing
         if self.transform:
             data_slice = self.transform(data_slice)
         if self.target_transform:
