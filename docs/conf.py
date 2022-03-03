ece39cca6530ab69d710331af94b4f601a5f1fa3
Gregor Lenz <mail@lenzgregor.com>
2022-03-03 11:27:59 +0100

    use pbr for automatic versioning

--- docs/conf.py
+++ docs/conf.py
@@ -1,36 +1,12 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
 import tonic
-
-# -- Project information -----------------------------------------------------
 
 project = "Tonic"
 copyright = "2019-2021, the neuromorphs of Telluride"
 author = "Gregor Lenz"
 
-version = ".".join(tonic.__version__.split("."))
-release = tonic.__version__
-
 master_doc = "index"
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "myst_nb"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "pbr.sphinxext", "myst_nb"]
 
 # Napoleon settings
 napoleon_google_docstring = True
@@ -49,23 +25,21 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-
 html_theme = "sphinx_book_theme"
+html_title = tonic.__version__
 html_logo = "_static/tonic-logo-black.png"
 html_favicon = "_static/tonic_favicon.png"
 html_show_sourcelink = True
 html_sourcelink_suffix = ""
 
 html_theme_options = {
-    "logo_only": True,
     "repository_url": "https://github.com/neuromorphs/tonic",
     "use_repository_button": True,
     "use_issues_button": True,
     "use_edit_page_button": True,
-    "use_fullscreen_button": False,
+    "repository_branch": "develop",
+    "path_to_docs": "docs",
+    "use_fullscreen_button": True,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-import setuptools
-import runpy
-import os
+from setuptools import setup
 
-root = os.path.dirname(os.path.realpath(__file__))
-version = runpy.run_path(os.path.join(root, "tonic", "version.py"))["version"]
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="tonic",
-    version=version,
-    author="The Neuromorphs of Telluride",
-    author_email="lenz.gregor@gmail.com",
-    description="Event-based datasets and transformations based on pyTorch vision.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/neuromorphs/tonic",
-    include_package_data=False,
-    packages=setuptools.find_packages(),
-    install_requires=["numpy", "h5py", "importRosbag>=1.0.3", "scipy", "tqdm", "typing_extensions", "librosa"],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Science/Research",
-    ],
-)
+setup(setup_requires=["pbr"], pbr=True)
