2d6bfe3e1e7e16d505ce3aa9bd67ab1fe3c6e585
Gregor Lenz <gregor.lenz@synsense.ai>
2022-03-17 21:32:10 +0100

    add automatic tonic.__version__ using pbr

--- tonic/__init__.py
+++ tonic/__init__.py
@@ -2,3 +2,7 @@ from . import datasets, transforms, collation, io, slicers, utils
 from .dataset import Dataset
 from .cached_dataset import CachedDataset, DiskCachedDataset, MemoryCachedDataset
 from .sliced_dataset import SlicedDataset
+from pbr.version import VersionInfo
+
+all = ('__version__')
+__version__ = VersionInfo('tonic').release_string()
