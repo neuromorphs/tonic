7f06b3daa143571d0da1092585ea8586cf67d045
Gregor Lenz <gregor.lenz@synsense.ai>
2022-06-24 10:27:37 +0200

    add plotting support for audio events

--- tonic/utils.py
+++ tonic/utils.py
@@ -29,27 +29,37 @@ def plot_event_grid(events, axis_array=(1, 3), plot_frame_number=False):
             " dependency."
         )
 
-    sensor_size_x = int(events["x"].max() + 1)
-    sensor_size_y = int(events["y"].max() + 1)
-    sensor_size_p = len(np.unique(events["p"]))
-    sensor_size = (sensor_size_x, sensor_size_y, sensor_size_p)
+    if "y" in events.dtype.names:
+        sensor_size_x = int(events["x"].max() + 1)
+        sensor_size_y = int(events["y"].max() + 1)
+        sensor_size_p = len(np.unique(events["p"]))
+        sensor_size = (sensor_size_x, sensor_size_y, sensor_size_p)
 
-    transform = transforms.ToFrame(
-        sensor_size=sensor_size, n_time_bins=np.product(axis_array)
-    )
+        transform = transforms.ToFrame(
+            sensor_size=sensor_size, n_time_bins=np.product(axis_array)
+        )
 
-    frames = transform(events)
-    fig, axes_array = plt.subplots(*axis_array)
+        frames = transform(events)
+        fig, axes_array = plt.subplots(*axis_array)
 
-    if 1 in axis_array:
-        axes_array = axes_array.reshape(1, -1)
+        if 1 in axis_array:
+            axes_array = axes_array.reshape(1, -1)
 
-    for i in range(axis_array[0]):
-        for j in range(axis_array[1]):
-            frame = frames[i * axis_array[1] + j]
-            axes_array[i, j].imshow(frame[1] - frame[0])
-            axes_array[i, j].axis("off")
-            if plot_frame_number:
-                axes_array[i, j].title.set_text(str(i * axis_array[1] + j))
-    plt.tight_layout()
-    plt.show()
+        for i in range(axis_array[0]):
+            for j in range(axis_array[1]):
+                frame = frames[i * axis_array[1] + j]
+                axes_array[i, j].imshow(frame[1] - frame[0])
+                axes_array[i, j].axis("off")
+                if plot_frame_number:
+                    axes_array[i, j].title.set_text(str(i * axis_array[1] + j))
+        plt.tight_layout()
+        plt.show()
+
+    else:
+        sensor_size_x = int(events["x"].max() + 1)
+        frame_transform = transforms.ToFrame(sensor_size=(sensor_size_x, 1, 1), n_time_bins=sensor_size_x*2)
+
+        frames = frame_transform(events)
+        plt.imshow(frames.squeeze().T)
+        plt.xlabel("Time")
+        plt.ylabel("Channels")
