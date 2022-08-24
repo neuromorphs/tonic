c5ced9f01532075015a1838ce3a641d17c327b50
Gregor Lenz <gregor.lenz@synsense.ai>
2022-08-24 12:16:28 +0200

    change list comprehension to generator comprehension in SlicedDataset

--- tonic/datasets/dsec.py
+++ tonic/datasets/dsec.py
@@ -282,7 +282,7 @@ class DSEC(Dataset):
 
                 png_filenames = list_files(full_base_folder, ".png", prefix=True)
                 target = np.array(
-                    [imageio.imread(file, format="PNG-FI") for file in png_filenames]
+                    [imageio.v2.imread(file, format="PNG-FI") for file in png_filenames]
                 )
                 target[:, :, :, :2] -= 2 ^ 15
                 target[:, :, :, :2] /= 128
