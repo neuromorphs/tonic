367b27f0133e8202812e9e04d2cc81702df4b536
Gregor Lenz <gregor.lenz@synsense.ai>
2023-01-12 10:46:57 +0100

    make prototype module and therefore torchdata dependency optional

--- tonic/__init__.py
+++ tonic/__init__.py
@@ -1,9 +1,14 @@
 from pbr.version import VersionInfo
 
-from . import collation, datasets, io, prototype, slicers, transforms, utils
+from . import collation, datasets, io, slicers, transforms, utils
 from .cached_dataset import CachedDataset, DiskCachedDataset, MemoryCachedDataset
 from .dataset import Dataset
 from .sliced_dataset import SlicedDataset
+
+try:
+    from . import prototype
+except (ImportError, NameError):
+    pass
 
 all = "__version__"
 __version__ = VersionInfo("tonic").release_string()
